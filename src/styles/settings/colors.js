66bd987d9567672ea5f715a787178368e21a997c
Rodrigo Reis <roreis99@gmail.com>
2019-10-23 15:02:27 -0300

    chore: config colors

--- src/styles/settings/colors.js
+++ src/styles/settings/colors.js
@@ -4,6 +4,10 @@ const GlobalColors = css`
   :root {
     --color-white: #fff;
     --color-black: #000;
+
+    --color-turquoise: #98c7bc;
+    --color-pink: #ff0072;
+    --color-pink-hovered: #c20056;
   }
 `;
 
