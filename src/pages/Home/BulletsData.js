e539be8b63cb6ccabb7b34d4cb8179e3d2613380
Rodrigo Reis <roreis99@gmail.com>
2019-10-25 15:32:07 -0300

    feat: add data to component

---
+++ src/pages/Home/BulletsData.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    name: 'pera',
+    src:
+      'https://ogimg.infoglobo.com.br/in/2700613-827-06a/FT1086A/652/xBob-Esponja-Reproducao.jpg.pagespeed.ic.Yu09BGtxU5.jpg',
+    link: '/',
+    alt: 'foto teste',
+  },
+  {
+    name: 'pera',
+    src:
+      'https://ogimg.infoglobo.com.br/in/2700613-827-06a/FT1086A/652/xBob-Esponja-Reproducao.jpg.pagespeed.ic.Yu09BGtxU5.jpg',
+    link: '/',
+    alt: 'foto teste',
+  },
+  {
+    name: 'pera',
+    src:
+      'https://ogimg.infoglobo.com.br/in/2700613-827-06a/FT1086A/652/xBob-Esponja-Reproducao.jpg.pagespeed.ic.Yu09BGtxU5.jpg',
+    link: '/',
+    alt: 'foto teste',
+  },
+  {
+    name: 'pera',
+    src:
+      'https://ogimg.infoglobo.com.br/in/2700613-827-06a/FT1086A/652/xBob-Esponja-Reproducao.jpg.pagespeed.ic.Yu09BGtxU5.jpg',
+    link: '/',
+    alt: 'foto teste',
+  },
+];
