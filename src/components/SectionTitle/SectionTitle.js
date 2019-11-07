78e3fbc001c4ee2f3668ba7387edc7220c4f0aaa
Rodrigo Reis <roreis99@gmail.com>
2019-11-07 16:27:45 -0300

    feat: create SectionTitle component

---
+++ src/components/SectionTitle/SectionTitle.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Container from './styles';
+
+const ContainerSpaced = ({ children }) => (
+  <Container>{children}</Container>
+);
+export default ContainerSpaced;
